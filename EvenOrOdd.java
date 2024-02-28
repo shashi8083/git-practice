976336fe3a6d55701b0d80b74567ea34f9c4d3bf
shashibhushan <bshashi8083@gmail.com>
2024-02-28 22:51:16 +0530

    Successfully added

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class EvenOrOdd
+{
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	if(n%2 == 0)
+	System.out.println("Number is even ");
+	else
+	System.out.println("Number is odd");
+	}
+}
